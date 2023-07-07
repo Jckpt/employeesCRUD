50fcdb29b1ed4841a1cc32bfec1aab9482d7748e
jckpt <krawcu333@gmail.com>
2023-07-07 13:27:25 +0200

    test

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <div className="min-h-screen">
       <header className="h-16 w-full bg-green-400">siema</header>
-      <main className="flex flex-col items-center justify-between p-2 md:p-24">
+      <main className="flex flex-col items-center justify-center p-2 md:p-24">
         <EmployeeTable />
       </main>
     </div>
